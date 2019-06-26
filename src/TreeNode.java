914457dad3d7d06c143191bb0a76cdb6013146ff
Dymon Moore <dmoore12@villanova.edu>
2019-06-25 22:23:11 -0400

    Implemented Print function

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,14 @@
-
 public class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+	public TreeNode(int data, TreeNode left, TreeNode right) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+	
+	
 
 }
+
