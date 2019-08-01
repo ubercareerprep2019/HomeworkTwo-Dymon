9e10912471d40b481e50a85a9df4ede50794af11
Dymon Moore <dmoore12@villanova.edu>
2019-08-01 01:46:48 -0400

    Fix:TreeNode made into template class to allow other types of input data.

--- src/Tree.java
+++ src/Tree.java
@@ -1,38 +1,34 @@
 public class Tree {
 	static TreeNode root;
-	
+
 	public Tree(TreeNode root) {
 		this.root = root;
 	}
 
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		TreeNode leftgrandChild = new TreeNode(6, null, null);
 		TreeNode rightgrandChild = new TreeNode(3, null, null);
 		TreeNode leftChild = new TreeNode(5, null, null);
-		TreeNode rightChild = new TreeNode(2, rightgrandChild,leftgrandChild);
+		TreeNode rightChild = new TreeNode(2, rightgrandChild, leftgrandChild);
 		TreeNode left = new TreeNode(7, null, null);
 		TreeNode right = new TreeNode(17, leftChild, rightChild);
 		TreeNode root = new TreeNode(1, left, right);
 		Tree tree = new Tree(root);
-		
+
 		print(root);
-		
-	
+
 	}
-	
-	
-	public static void print(TreeNode root) 
-	{
-		TreeNode rootNode  = root;
-		
-		if(rootNode == null) //Base case
+
+	public static void print(TreeNode root) {
+		TreeNode rootNode = root;
+
+		if (rootNode == null) // Base case
 		{
-			
-		}else {
-	
+
+		} else {
+
 			System.out.println(rootNode.data);
 			rootNode = root.left;
 			print(rootNode);
